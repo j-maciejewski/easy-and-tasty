77ce440c6d1b872cfd5e69af175ab3d503d2ed03
j-maciejewski <j.maciejewski.kontakt@gmail.com>
2026-03-03 22:50:14 +0100

    fix: add dynamic export for dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -17,6 +17,8 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon-dashboard.png" }],
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function ({ children }: React.PropsWithChildren) {
   const categories = await api.authorized.category.getCategories();
   const cuisines = await api.authorized.cuisine.getCuisines();
